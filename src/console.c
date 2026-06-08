dedd9b1cdfd33f3d1e1adb6c4577da47443e594f
Cheezyun <121702172+HermosMerlin@users.noreply.github.com>
2026-06-09 01:06:21 +0800

    Fix console initialization behavior

--- src/console.c
+++ src/console.c
@@ -1,6 +1,5 @@
 #include "console.h"
 
-#include <stdio.h>
 #include <windows.h>
 
 void console_init(int cols, int rows) {
@@ -16,6 +15,24 @@ void console_init(int cols, int rows) {
         rows = 1;
     }
 
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    CONSOLE_SCREEN_BUFFER_INFO info;
+    if (GetConsoleScreenBufferInfo(output, &info)) {
+        int current_cols = info.srWindow.Right - info.srWindow.Left + 1;
+        int current_rows = info.srWindow.Bottom - info.srWindow.Top + 1;
+
+        if (cols < current_cols || rows < current_rows) {
+            SMALL_RECT shrink_rect;
+            shrink_rect.Left = 0;
+            shrink_rect.Top = 0;
+            shrink_rect.Right = (SHORT)(cols - 1);
+            shrink_rect.Bottom = (SHORT)(rows - 1);
+            SetConsoleWindowInfo(output, TRUE, &shrink_rect);
+        }
+    }
+
     COORD buffer_size;
     buffer_size.X = (SHORT)cols;
     buffer_size.Y = (SHORT)rows;
@@ -28,6 +45,7 @@ void console_init(int cols, int rows) {
 
     SetConsoleScreenBufferSize(output, buffer_size);
     SetConsoleWindowInfo(output, TRUE, &window_rect);
+    SetConsoleCursorPosition(output, (COORD){0, 0});
 }
 
 void console_clear(void) {
@@ -43,7 +61,7 @@ void console_clear(void) {
 
     DWORD cell_count = (DWORD)info.dwSize.X * (DWORD)info.dwSize.Y;
     DWORD written = 0;
-    COORD home = { 0, 0 };
+    COORD home = {0, 0};
 
     FillConsoleOutputCharacterA(output, ' ', cell_count, home, &written);
     FillConsoleOutputAttribute(output, info.wAttributes, cell_count, home, &written);
@@ -56,6 +74,6 @@ void console_move_home(void) {
         return;
     }
 
-    COORD home = { 0, 0 };
+    COORD home = {0, 0};
     SetConsoleCursorPosition(output, home);
 }
