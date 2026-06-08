d7818e97fce7f2d3de28d697e82729d2f07fca78
Cheezyun <121702172+HermosMerlin@users.noreply.github.com>
2026-06-09 00:34:13 +0800

    Add console control helpers

---
+++ include/console.h
@@ -0,0 +1,8 @@
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+void console_init(int cols, int rows);
+void console_clear(void);
+void console_move_home(void);
+
+#endif
---
+++ src/console.c
@@ -0,0 +1,61 @@
+#include "console.h"
+
+#include <stdio.h>
+#include <windows.h>
+
+void console_init(int cols, int rows) {
+    HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (output == INVALID_HANDLE_VALUE) {
+        return;
+    }
+
+    if (cols < 1) {
+        cols = 1;
+    }
+    if (rows < 1) {
+        rows = 1;
+    }
+
+    COORD buffer_size;
+    buffer_size.X = (SHORT)cols;
+    buffer_size.Y = (SHORT)rows;
+
+    SMALL_RECT window_rect;
+    window_rect.Left = 0;
+    window_rect.Top = 0;
+    window_rect.Right = (SHORT)(cols - 1);
+    window_rect.Bottom = (SHORT)(rows - 1);
+
+    SetConsoleScreenBufferSize(output, buffer_size);
+    SetConsoleWindowInfo(output, TRUE, &window_rect);
+}
+
+void console_clear(void) {
+    HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (output == INVALID_HANDLE_VALUE) {
+        return;
+    }
+
+    CONSOLE_SCREEN_BUFFER_INFO info;
+    if (!GetConsoleScreenBufferInfo(output, &info)) {
+        return;
+    }
+
+    DWORD cell_count = (DWORD)info.dwSize.X * (DWORD)info.dwSize.Y;
+    DWORD written = 0;
+    COORD home = { 0, 0 };
+
+    FillConsoleOutputCharacterA(output, ' ', cell_count, home, &written);
+    FillConsoleOutputAttribute(output, info.wAttributes, cell_count, home, &written);
+    SetConsoleCursorPosition(output, home);
+}
+
+void console_move_home(void) {
+    HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (output == INVALID_HANDLE_VALUE) {
+        return;
+    }
+
+    COORD home = { 0, 0 };
+    SetConsoleCursorPosition(output, home);
+}
